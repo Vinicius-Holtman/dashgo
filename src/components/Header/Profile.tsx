9bd33e9805d8434e61fb68d6ce9af12f8edf7d2c
Vinicius <viniholtman28@gmail.com>
2022-05-19 14:38:00 -0300

    Apply conditional hidden for text

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Vinicius Holtman</Text>
-        <Text color="gray.300" fontSize="small">viniholtman28@gmail.com</Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Vinicius Holtman</Text>
+          <Text color="gray.300" fontSize="small">viniholtman28@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Vinicius Holtman" src="https://github.com/Vinicius-Holtman.png" />
     </Flex>
