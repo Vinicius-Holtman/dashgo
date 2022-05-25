6db84be090db3dcbf8aafd8e1ddea2fe2a037afb
Vinicius <viniholtman28@gmail.com>
2022-05-25 10:18:39 -0300

    export qeury client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { makeServer } from '../services/mirage'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '../services/queryClient'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContent'
 import { theme } from '../styles/theme'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
-
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
